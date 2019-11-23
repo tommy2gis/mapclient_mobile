a1c92285b54dc24424ecc8fbf6f872ef379c42fb
shitao <shitao19880316@gmail.com>
2019-11-23 21:38:48 +0800

    update

--- src/components/mobiles/DataCollect.jsx
+++ src/components/mobiles/DataCollect.jsx
@@ -1,41 +1,60 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { Button,TextareaItem,Picker, NavBar,List, InputItem } from "antd-mobile";
-import {changeModel} from "../../actions/map";
+import {
+  Button,
+  TextareaItem,
+  Picker,
+  Toast,
+  NavBar,
+  List,
+  InputItem
+} from "antd-mobile";
+import { changeModel } from "../../actions/map";
 import { createForm } from "rc-form";
-import photopng from './assets/photo.png'
-const  types=[{
-    "value": "340000",
-    "label": "安徽省"
-    }]
+import photopng from "./assets/photo.png";
+const types = [
+  {
+    value: "340000",
+    label: "安徽省"
+  }
+];
 
 class DataCollect extends Component {
   _onDataClick = () => {
     this.props.history.push("/");
     this.props.changeModel("dataedit");
   };
-  submit=()=>{
-    return axios.post(ServerUrl + "/acquisition/dataInformation/save",{
-      "usrId":userName,
-      "name": passWord,
-      "geo":'',
-      "content":'',
-      "state":0,
-      "track":'',
-      "trackLength":'',
-      "projectId":'',
-      "patternSpotName":'',
-      "patternSpotGeo":'',
-      "describes":'',
-      "tskClaId":''
-  }).then((response) => {
-
-
-  }).catch((e) => {
-      //message.warning('提交数据失败,请稍后再试');
-  });
-  }
+  submit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        axios.post(ServerUrl + "/acquisition/dataInformation/save", {
+            usrId: userName,
+            name: values.name,
+            geo: "",
+            content: values.content,
+            state: 0,
+            track: "",
+            trackLength: "",
+            projectId: values.projectId,
+            patternSpotName: values.patternSpotName,
+            patternSpotGeo: "",
+            tskClaId: values.tskClaId
+          })
+          .then(response => {})
+          .catch(e => {
+            //message.warning('提交数据失败,请稍后再试');
+          });
+      } else {
+        for (const key in err) {
+          const el = err[key];
+          Toast.info(el.errors[0].message, 1);
+          return;
+        }
+      }
+    });
+  };
 
   render() {
     const { getFieldProps } = this.props.form;
@@ -49,64 +68,72 @@ class DataCollect extends Component {
               <a className="back-main"></a>
             </div>
           }
-          rightContent={
-            <div onClick={this._onDataClick}>
-              地图
-            </div>
-          }
-          
+          rightContent={<div onClick={this._onDataClick}>地图</div>}
         >
-          
           数据采集
         </NavBar>
         <Picker
           data={types}
           cols={1}
-          {...getFieldProps("district3")}
+          {...getFieldProps("city", {
+            rules: [{ required: true, message: "请选择城市名称!" }]
+          })}
           className="forss"
         >
-          <List.Item arrow="horizontal">城市名称</List.Item>
+          <List.Item arrow="horizontal">城市名称<span style={{ color: "red" }}>*</span></List.Item>
         </Picker>
         <Picker
           data={types}
           cols={1}
-          {...getFieldProps("district3")}
+          {...getFieldProps("town", {
+            rules: [{ required: true, message: "请选择县级名称!" }]
+          })}
           className="forss"
         >
-          <List.Item arrow="horizontal">县级名称</List.Item>
+          <List.Item arrow="horizontal">县级名称<span style={{ color: "red" }}>*</span></List.Item>
         </Picker>
         <Picker
           data={types}
           cols={1}
-          {...getFieldProps("district3")}
+          {...getFieldProps("projectId", {
+            rules: [{ required: true, message: "请输入项目名称!" }]
+          })}
           className="forss"
         >
-          <List.Item arrow="horizontal">项目名称</List.Item>
+          <List.Item arrow="horizontal">项目名称<span style={{ color: "red" }}>*</span></List.Item>
         </Picker>
         <InputItem
-          {...getFieldProps("autofocus")}
+          {...getFieldProps("patternSpotName", {
+            rules: [{ required: true, message: "请输入图斑名称!" }]
+          })}
           clear
           placeholder="请输入图斑名称"
           ref={el => (this.autoFocusInst = el)}
         >
-          图斑名称
+          图斑名称<span style={{ color: "red" }}>*</span>
         </InputItem>
         <InputItem
-          {...getFieldProps("autofocus")}
+          {...getFieldProps("name", {
+            rules: [{ required: true, message: "请输入数据名称!" }]
+          })}
           clear
           placeholder="请输入数据名称"
           ref={el => (this.autoFocusInst = el)}
         >
-          数据名称
+          数据名称<span style={{ color: "red" }}>*</span>
         </InputItem>
         <TextareaItem
-          {...getFieldProps("note1")}
+          {...getFieldProps("content", {
+            rules: [{ required: true, message: "请输入备注信息!" }]
+          })}
           rows={3}
-          title="备注信息"
+          title={<div>
+            备注信息<span style={{ color: "red" }}>*</span>
+          </div>}
           placeholder="请输入备注信息"
         ></TextareaItem>
         <InputItem
-          {...getFieldProps("autofocus")}
+          {...getFieldProps("tskClaId")}
           clear
           placeholder="请输入数据类型"
           ref={el => (this.autoFocusInst = el)}
@@ -117,20 +144,17 @@ class DataCollect extends Component {
           <div className="am-list-line">
             <div className="am-input-label am-input-label-5">添加照片</div>
             <div className="am-input-control">
-                <img src={photopng}></img>
+              <img src={photopng}></img>
             </div>
           </div>
         </div>
-        <Button
-          className="taskroad-btn"  onClick={this.submit}
-        >
+        <Button className="taskroad-btn" onClick={this.submit}>
           提交
         </Button>
       </div>
     );
   }
 }
-
 
 export default connect(
   state => {
@@ -140,4 +164,3 @@ export default connect(
     changeModel
   }
 )(createForm()(DataCollect));
-
--- src/components/mobiles/TaskCollect.jsx
+++ src/components/mobiles/TaskCollect.jsx
@@ -1,34 +1,94 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Button,TextareaItem,Picker, NavBar,List, InputItem } from "antd-mobile";
+import {
+  Button,
+  TextareaItem,
+  Picker,
+  NavBar,
+  Toast,
+  InputItem
+} from "antd-mobile";
 import { createForm } from "rc-form";
-import photopng from './assets/photo.png'
-const  types=[{
-    "value": "340000",
-    "label": "安徽省"
-    }]
+import photopng from "./assets/photo.png";
+import wx from "weixin-js-sdk";
+const types = [
+  {
+    value: "340000",
+    label: "安徽省"
+  }
+];
 
 class TaskCollect extends Component {
-  submit=()=>{
-    return axios.post(ServerUrl + "/acquisition/dataInformation/save",{
-      "usrId":userName,
-      "name": passWord,
-      "geo":'',
-      "content":'',
-      "state":0,
-      "type":'',
-      "tskId":'',
-      "fileTemporaryNames":'',
-      "fileNames":''
+  submit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        axios
+          .post(ServerUrl + "/acquisition/dataInformation/save", {
+            usrId: userName,
+            name: values.name,
+            geo: "",
+            content: values.content,
+            state: 0,
+            type: values.type,
+            tskId: "",
+            fileTemporaryNames: "",
+            fileNames: ""
+          })
+          .then(response => {})
+          .catch(e => {
+            //message.warning('提交数据失败,请稍后再试');
+          });
+      } else {
+        for (const key in err) {
+          const el = err[key];
+          Toast.info(el.errors[0].message, 1);
+          return;
+        }
+      }
+    });
+  };
 
+  uploadPhotos = images => {
+    if (images.localId.length == 0) {
+      return;
+    }
+    var i = 0,
+      length = images.localId.length;
+    images.serverId = [];
+    function upload() {
+      wx.uploadImage({
+        localId: images.localId[i],
+        isShowProgressTips: 1,
+        success: function(res) {
+          i++;
+          //alert('已上传：' + i + '/' + length);
+          images.serverId.push(res.serverId);
+          if (i < length) {
+            upload();
+          }
+        },
+        fail: function(res) {
+          alert(JSON.stringify(res));
+        }
+      });
+    }
+    upload();
+  };
 
-  }).then((response) => {
-
-
-  }).catch((e) => {
-      //message.warning('提交数据失败,请稍后再试');
-  });
-  }
+  getPhoto = () => {
+    var images = {
+      localId: [],
+      serverId: []
+    };
+    wx.chooseImage({
+      success: res => {
+        images.localId = res.localIds;
+        this.uploadPhotos(images);
+        alert("已选择 " + res.localIds.length + " 张图片");
+      }
+    });
+  };
 
   render() {
     const { getFieldProps } = this.props.form;
@@ -46,38 +106,46 @@ class TaskCollect extends Component {
           任务数据采集
         </NavBar>
         <InputItem
-          {...getFieldProps("autofocus")}
+          {...getFieldProps("name", {
+            rules: [{ required: true, message: "请输入数据名称!" }]
+          })}
           clear
           placeholder="请输入数据名称"
           ref={el => (this.autoFocusInst = el)}
         >
-          数据名称
+          数据名称<span style={{ color: "red" }}>*</span>
         </InputItem>
         <TextareaItem
-          {...getFieldProps("note1")}
+          {...getFieldProps("content", {
+            rules: [{ required: true, message: "请输入备注信息!" }]
+          })}
           rows={3}
-          title="备注信息"
+          title={
+            <div>
+              备注信息<span style={{ color: "red" }}>*</span>
+            </div>
+          }
           placeholder="请输入备注信息"
         ></TextareaItem>
-        <Picker
-          data={types}
-          cols={1}
-          {...getFieldProps("district3")}
-          className="forss"
+        <InputItem
+          {...getFieldProps("type")}
+          clear
+          placeholder="请输入数据类型"
+          ref={el => (this.autoFocusInst = el)}
         >
-          <List.Item arrow="horizontal">数据类型</List.Item>
-        </Picker>
+          数据类型
+        </InputItem>
         <div className="am-list-item am-input-item am-list-item-middle">
           <div className="am-list-line">
-            <div className="am-input-label am-input-label-5">添加照片</div>
-            <div className="am-input-control">
-                <img src={photopng}></img>
+            <div className="am-input-label am-input-label-5">
+              添加照片<span style={{ color: "red" }}>*</span>
+            </div>
+            <div className="am-input-control" onClick={this.getPhoto}>
+              <img src={photopng}></img>
             </div>
           </div>
         </div>
-        <Button onClick={this.submit}
-          className="taskroad-btn"
-        >
+        <Button onClick={this.submit} className="taskroad-btn">
           提交
         </Button>
       </div>
